f42b787cf82617d477390ffcb466d059255784dd
Jason Rowe <jason.rowe@gmail.com>
2011-12-31 14:50:18 -0600

    change namespace

--- src/com/alexgames/BabyScroll.java
+++ src/com/alexgames/BabyScroll.java
@@ -1,15 +1,15 @@
-package com.example;
+package com.alexgames;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity
+public class BabyScroll extends Activity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.baby_scroll_layout);
     }
 }
